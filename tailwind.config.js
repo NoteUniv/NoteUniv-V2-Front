7e1cfc520351f7b46b70a3d3daecfbd3e021c8d3
GUINGAND SOLENE <solene.guingand@etu.unistra.fr>
2022-03-11 09:01:59 +0100

    correction color classss name

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,12 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        'nu-primary': '#110133',
-        'nu-secondary': '#2F80ED',
-        'nu-green': '#33DB92',
-        'nu-orange': 'orange',
-        'nu-red': '#fe4c6a',
-        'nu-gray': {
+        'nuPrimary': '#110133',
+        'nuSecondary': '#2F80ED',
+        'nuGreen': '#33DB92',
+        'nuOrange': 'orange',
+        'nuRed': '#fe4c6a',
+        'nuGray': {
           100: '#F8F9FB',
           200: '#E6E6E6',
           300: '#C4C4C4',
--- views/parts/sidebar.php
+++ views/parts/sidebar.php
@@ -1,8 +1,8 @@
 <aside class="fixed top-0 w-1/6 bg-white h-screen shadow-drop">
-  <div class="bg-primary w-full aspect-square flex align-center justify-center">
+  <div class="bg-nuPrimary w-full aspect-square flex align-center justify-center">
     <img class="w-3/4" src="./src/assets/images/logo.svg" alt="">
   </div>
-  <select name="semesters" id="smester-select" class="bg-secondary w-full text-white p-3">
+  <select name="semesters" id="smester-select" class="bg-nuSecondary w-full text-white p-3">
     <option value="1">Semestre 1</option>
     <option value="2">Semestre 2</option>
     <option value="3">Semestre 3</option>
@@ -13,7 +13,7 @@
   <div class="bg-third w-full p-5 text-center">
     <p>Moyenne générale</p>
     <div>
-      <span>14,95</span>
+      <span class="text-nu-grenn">14,95</span>
       <span>/20</span>
     </div>
   </div>
