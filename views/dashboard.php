e086c1851e8227af4b61f4148e30bf8513f5a231
YacovaHashigori <eloiantz@gmail.com>
2022-03-14 15:59:13 +0100

    update: dashbord styles

--- views/dashboard.php
+++ views/dashboard.php
@@ -1,17 +1,16 @@
 <div class="grid grid-cols-10 gap-4 mb-28">
     <div class="relative col-start-2 col-span-8 flex gap-4">
         <div class="box box--big flex-grow">
-            <div class="flex justify-between items-center mb-2">
+            <div class="flex justify-between items-center mb-6">
                 <h2 class="title title--underline">Last grades</h2>
                 <a href="" class="btn-link">
                     <span>View all my grades</span>
                 </a>
             </div>
-            <div class="relative">
-                <div class="sticky top-0 h-6 bg-white w-full outline outline-1 outline-white"></div>
+            <div>
                 <table class="w-full border-collapse border-l border-b border-r border-nu-gray-200">
                     <thead>
-                        <tr class="sticky top-6 h-12 bg-nu-primary text-sm text-white">
+                        <tr class="h-12 bg-nu-primary text-sm text-white">
                             <th class="font-semibold text-left pl-4">Subject</th>
                             <th class="font-semibold text-left pl-4">Name of work</th>
                             <th class="font-semibold">My grade</th>
@@ -19,7 +18,7 @@
                         </tr>
                     </thead>
                     <tbody>
-                        <?php for ($i = 0; $i < 6; $i++) : ?>
+                        <?php for ($i = 0; $i < 5; $i++) : ?>
                         <tr class="h-12 border-b border-nu-gray-200 text-sm">
                             <td class="pl-4">com</td>
                             <td class="pl-4">Contrôle Comportement ...</td>
